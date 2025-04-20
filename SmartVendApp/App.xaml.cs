eb46f03a889b9744e515b51f062b03f979243fba
bogorski <mateuszbogorski@gmail.com>
2025-04-20 13:43:39 +0200

    add maui blazor, remove maui xaml and xamarin

---
+++ SmartVendApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartVendApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ SmartVendApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartVendApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
